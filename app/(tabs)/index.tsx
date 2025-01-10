176931b58e308a399b9f9ca2106a31cfcdd2dafc
yuotsubo <513047.otsubo@gmail.com>
2025-01-10 23:37:04 +0900

    add imageViewer in Home Screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,14 @@
 import { Text, View, StyleSheet } from "react-native";
-import { Link } from "expo-router";
+import ImageViewer from "@/components/ImageViewer";
+
+const PlaceholderImage = require('@/assets/images/background-image.png');
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Home screen</Text>
-			<Link href="/about" style={styles.button}>
-				Go to About Screen
-			</Link>
+      <View style={styles.imageContainer}>
+				<ImageViewer imgSource={PlaceholderImage} />
+			</View>
     </View>
   );
 }
@@ -19,13 +20,8 @@ const styles = StyleSheet.create({
 		alignItems: 'center',
 		justifyContent: 'center',
 	},
-	text: {
-		color: '#fff',
-	},
-	button: {
-		fontSize: 20,
-		textDecorationLine: 'underline',
-		color: '#fff',
+	imageContainer: {
+		flex: 1,
 	},
 });
 
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,7 +4,7 @@ export default function RootLayout() {
   return (
 		<Stack>
 			<Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-			<Stack.Screen name="name" />
+			<Stack.Screen name="+not-found" />
 		</Stack>
 	);
 }
