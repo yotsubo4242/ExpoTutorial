77857fc6f1a001723616055c54ef45f6f98fb50c
yuotsubo <513047.otsubo@gmail.com>
2025-01-11 18:29:56 +0900

    customize the button component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,6 @@
 import { Text, View, StyleSheet } from "react-native";
 import ImageViewer from "@/components/ImageViewer";
+import Button from "@/components/Button";
 
 const PlaceholderImage = require('@/assets/images/background-image.png');
 
@@ -8,6 +9,10 @@ export default function Index() {
     <View style={styles.container}>
       <View style={styles.imageContainer}>
 				<ImageViewer imgSource={PlaceholderImage} />
+			</View>
+			<View style={styles.footerContainer}>
+				<Button theme="primary" label="Choose a photo" />
+				<Button label="Use this photo" />
 			</View>
     </View>
   );
@@ -22,6 +27,10 @@ const styles = StyleSheet.create({
 	},
 	imageContainer: {
 		flex: 1,
+	},
+	footerContainer: {
+		flex: 1 / 3,
+		alignItems: 'center',
 	},
 });
 
--- components/Button.tsx
+++ components/Button.tsx
@@ -1,11 +1,29 @@
 import { StyleSheet, View, Pressable, Text } from "react-native";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
 
 type Props = {
 	label: string;
+	theme?: 'primary';
 };
 
-export default function Button({ label }: Props) {
-	return (
+export default function Button({ label, theme }: Props) {
+	if (theme === 'primary')
+		return(
+			<View
+				style={[
+					styles.buttonContainer,
+					{ borderWidth: 4, borderColor: '#ffd33d', borderRadius: 18 },
+				]}>
+				<Pressable
+					style={[styles.button, { backgroundColor: '#fff' }]}
+					onPress={() => alert('You pressed a button.')}>
+					<FontAwesome name="picture-o" size={18} color="#25292e" style={styles.buttonIcon} />
+					<Text style={[styles.buttonLabel, { color: '#25292e' }]}>{label}</Text>
+				</Pressable>
+			</View>
+		);
+	
+		return (
 		<View style={styles.buttonContainer} >
 			<Pressable style={styles.button} onPress={() => alert('You pressed a button.')}>
 				<Text style={styles.buttonLabel}>{label}</Text>
@@ -30,6 +48,9 @@ const styles = StyleSheet.create({
 		alignItems: 'center',
 		justifyContent: 'center',
 		flexDirection: 'row',
+	},
+	buttonIcon: {
+		paddingRight: 8,
 	},
 	buttonLabel: {
 		color: '#fff',
